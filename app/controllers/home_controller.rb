524f2b3c5d08a3a43025dc86cab41a129e09a12a
udhayakumar <udhayakumar.dhanabalan@gmail.com>
2014-05-08 18:04:09 -0700

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+class HomeController < ApplicationController
+		
+		def show
+		end
+		
+		def about
+		end
+		
+		def contact
+		end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KentonBoutwell::Application.config.secret_token = 'e110c5490095f284239538ac194d375abc1225a50e8c2f335d1cd0b9b9e85278a6e31b90dc974924a416780052606b427d91beed75659caca9a99fa59b9fbb68'
